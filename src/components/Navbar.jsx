d5b496527a000e18ff479463ee2871b81aaa25c8
IAbdulSubhan <subhankhalidofficial@gmail.com>
2024-07-29 22:04:09 +0500

    Navbar created handled search params

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import NavbarItem from "./NavbarItem";
+
+
+const Navbar = () => {
+  return (
+    <div className="flex dark:bg-gray-800 bg-amber-200 p-4 lg:text-lg justify-center gap-8 ">
+      <NavbarItem title = "Trending" param = "fetchTrending"/>
+      <NavbarItem title = "Top Rated" param = "fetchTopRated"/>
+
+    </div>
+  )
+}
+
+export default Navbar
