456b48d627331e927540134bbf5a1deb6d4cb030
IAbdulSubhan <subhankhalidofficial@gmail.com>
2024-07-29 14:12:46 +0500

    added about page and its dummy content

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+// when ever you want create a page where you need to route in next js you have to make the folder for that respective page
+
+
+const About = () => {
+    return (
+        <div className="max-w-6xl mx-auto p-3 space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+            <p className="mt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+                distinctio natus. Voluptatem dolores recusandae unde itaque sequi impedit voluptatibus? Suscipit quos ipsum libero accusamus iste porro fuga placeat illo. Nobis.
+            </p>
+            <p className="mt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis magni, praesentium, eum architecto sed inventore fuga quasi laboriosam repellat molestias illum atque, perspiciatis itaque officiis laborum nostrum in aliquam et?
+                
+            </p>
+            <p className="mt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, tempore qui voluptatibus, corrupti vel, ducimus officiis dolor ipsa illum id optio. Maxime aperiam nesciunt nemo architecto ipsam recusandae. Eaque, accusamus?
+                Recusandae obcaecati cumque, iusto temporibus sapiente, molestiae at harum a voluptate repellendus quisquam atque eius eligendi consectetur omnis eveniet repudiandae velit iure ad culpa sunt, cum architecto blanditiis! Dolor, inventore!
+            </p>
+
+        </div>
+    )
+}
+
+export default About
